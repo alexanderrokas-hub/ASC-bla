115b223e1025ae2876747895167b300cb75c7d63
Alex <alexanderrokas@gmail.com>
2025-10-14 17:34:02 +0200

    first commit

---
+++ bild2/demos/CMakeFiles/demo_vector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_vector.dir/demo_vector.cpp.o"
+  "CMakeFiles/demo_vector.dir/demo_vector.cpp.o.d"
+  "demo_vector"
+  "demo_vector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bild2/demos/CMakeFiles/demo_vector.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for demo_vector.
+# This may be replaced when dependencies are built.
